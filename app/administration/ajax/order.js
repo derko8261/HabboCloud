38c9581fddea237753b499d78ad0c9e0e2f639b1
derko8261 <47055036+derko8261@users.noreply.github.com>
2021-03-04 16:42:50 +0100

    Add files via upload

--- app/administration/ajax/order.js
+++ app/administration/ajax/order.js
@@ -1,140 +1,23 @@
 $(document).ready(function() {
-    $('#hc_order_waiting').on('submit', function(e) {
+    $('#hc_order').on('submit', function(e) {
         e.preventDefault();
 
         var $this = $(this);
 
         $.ajax({
-            url: '../../../system/ajax/admin_order_waiting/'+name+'/'+extension+'/'+solution+'/'+cms+'/'+emulator+'/'+sso+'',
+            url: '../../../system/ajax/order',
             type: 'POST',
             data: $this.serialize(),
             dataType: 'json',
 			success: function(json){
 				if(json.status === 'success') {
-					$('#error').html('<div class="alert alert-success">'+json.response+'</div>');
+					$('#error').html('<div style="margin-top: -40px; margin-bottom: 80px;" class="alert bg-success">'+json.response+'</div></div>');
+					$('#name').val('');
 					window.setTimeout(function() {
-                        window.location = "/administration/commandes/waiting";
+                        window.location = "/Client/Tracking_Control";
                     }, 1500);
 				} else {
-					$('#error').html('<div class="alert alert-danger">'+json.response+'</div>');
-					setTimeout(function(){
-						$('#error').empty();
-					}, 2000);
-				}
-		   }
-
-        });
-    });
-});
-
-$(document).ready(function() {
-    $('#hc_order_actives').on('submit', function(e) {
-        e.preventDefault();
-
-        var $this = $(this);
-
-        $.ajax({
-            url: '../../../system/ajax/admin_order_actives/'+name+'/'+extension+'/'+sso+'',
-            type: 'POST',
-            data: $this.serialize(),
-            dataType: 'json',
-			success: function(json){
-				if(json.status === 'success') {
-					$('#error').html('<div class="alert alert-success">'+json.response+'</div>');
-					window.setTimeout(function() {
-                        window.location = "/administration/commandes/actives";
-                    }, 1500);
-				} else {
-					$('#error').html('<div class="alert alert-danger">'+json.response+'</div>');
-					setTimeout(function(){
-						$('#error').empty();
-					}, 2000);
-				}
-		   }
-
-        });
-    });
-});
-
-$(document).ready(function() {
-    $('#hc_order_refund').on('click', function(e) {
-        e.preventDefault();
-
-        var $this = $(this);
-
-        $.ajax({
-            url: '../../../system/ajax/admin_order_refund/'+name+'/'+extension+'/'+solution+'/'+sso+'',
-            type: 'POST',
-            data: $this.serialize(),
-            dataType: 'json',
-			success: function(json){
-				if(json.status === 'success') {
-					$('#error').html('<div class="alert alert-success">'+json.response+'</div>');
-					window.setTimeout(function() {
-                        window.location = "/administration/";
-                    }, 1500);
-				} else {
-					$('#error').html('<div class="alert alert-danger">'+json.response+'</div>');
-					setTimeout(function(){
-						$('#error').empty();
-					}, 2000);
-				}
-		   }
-
-        });
-    });
-});
-
-$(document).ready(function() {
-    $('.supprsusp').on('click', function(e) {
-        e.preventDefault();
-
-        var $this = $(this);
-		var idretro = $this.attr('id');
-		
-        $.ajax({
-            url: '../../../system/ajax/admin_order_suspended/'+idretro+'',
-            type: 'POST',
-            data: $this.serialize(),
-            dataType: 'json',
-			success: function(json){
-				if(json.status === 'success') {
-					$('#error').html('<div class="alert alert-success">'+json.response+'</div>');
-					window.setTimeout(function() {
-                        window.location = "/administration/commandes/suspended";
-                    }, 1500);
-				} else {
-					$('#error').html('<div class="alert alert-danger">'+json.response+'</div>');
-					setTimeout(function(){
-						$('#error').empty();
-					}, 2000);
-				}
-		   }
-
-        });
-    });
-});
-
-$(document).ready(function() {
-    $('#hc_order_swf').on('submit', function(e) {
-        e.preventDefault();
-
-        var $this = $(this);
-		var idretro = $this.attr('id');
-		
-        $.ajax({
-            url: '../../../system/ajax/admin_order_swf/'+name+'/'+extension+'/'+sso+'',
-            type: 'POST',
-            data: $this.serialize(),
-            dataType: 'json',
-			success: function(json){
-				if(json.status === 'success') {
-					$('#error').html('<div class="alert alert-success">'+json.response+'</div>');
-					window.setTimeout(function() {
-                        window.location = "/administration/commandes/swfs";
-                    }, 1500);
-				} else {
-					$('#error').html('<div class="alert alert-danger">'+json.response+'</div>');
+					$('#error').html('<div style="margin-top: -40px; margin-bottom: 80px;" class="alert bg-danger">'+json.response+'</div></div>');
 					setTimeout(function(){
 						$('#error').empty();
 					}, 2000);
